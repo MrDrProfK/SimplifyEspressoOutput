b1c5348651e22f511da5473b83ba80166a3e0262
MrDrProfK <38258014+MrDrProfK@users.noreply.github.com>
2018-11-08 01:14:05 -0500

    Users can now choose between 3 different modes of operation--simplified Espresso output file generation, combinational logic simulation, or a combined mode. Code was also refactored some, primarily for organizational purposes and readability.

--- simplify espresso output/main.cpp
+++ simplify espresso output/main.cpp
@@ -1,9 +1,10 @@
 //
 //  main.cpp
-//  simplify espresso output
+//  Simplify Espresso Output
 //
 //  Created by Aaron Knoll on 10/30/18.
-//  Copyright © 2018 Aaron Knoll. All rights reserved.
+//  Licensed under the GNU GPLv3
+//  (a copy of which is contained along with this application)
 //
 
 #include <iostream>
@@ -13,37 +14,43 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-
 using namespace std;
+
+string promptForRunMode();
+int simulateCombinationalLogic(int, int, int, vector<string>, vector<vector<string>>, vector<vector<string>>);
 
 int main(int argc, const char * argv[]) {
       
-      ifstream inputFileStream(argv[1]);
-      string inputLine;
-      string dataToBeOutputted;
-      int numInputs, numOutputs, numProducts;
+      int numInputs = 0, numOutputs = 0;
       vector<string> inputLabels;
       vector<vector<string>> outputLabels;
+      
+      ifstream inputFileStream(argv[1]);
+      string inputLine;
       while (getline(inputFileStream, inputLine)) {
             // line of espresso output file
             // create a more flexible structure for storing the espresso output
             stringstream s1(inputLine);
             
             string tempWord, firstWord;
+            // get context identifier
             s1 >> firstWord;
             
-            // test first word to determine context
+            // number of inputs
             if (!firstWord.compare(".i")) {
                   s1 >> numInputs;
             }
+            // number of outputs
             else if (!firstWord.compare(".o")) {
                   s1 >> numOutputs;
             }
+            // input labels
             else if (!firstWord.compare(".ilb")) {
                   while (s1 >> tempWord) {
                         inputLabels.push_back(tempWord);
                   }
             }
+            // output labels
             else if (!firstWord.compare(".ob")) {
                   while (s1 >> tempWord) {
                         vector<string> outputLabel;
@@ -51,10 +58,13 @@ int main(int argc, const char * argv[]) {
                         outputLabels.push_back(outputLabel);
                   }
             }
+            // products
             else if (!firstWord.compare(".p")) {
+                  int numProducts;
                   s1 >> numProducts;
                   
                   vector<vector<string>> products;
+                  string dataToBeOutputted;
                   for (int i = 0; i < numProducts; i++) {
                         getline(inputFileStream, inputLine);
                         string inputs, outputs;
@@ -80,6 +90,7 @@ int main(int argc, const char * argv[]) {
                               }
                         }
                   }
+                  
                   string prettyOutputs;
                   for (int i = 0; i < numOutputs; i++) {
                         prettyOutputs += outputLabels[i][0] + ":";
@@ -88,75 +99,103 @@ int main(int argc, const char * argv[]) {
                         }
                         prettyOutputs += "\n";
                   }
-                  dataToBeOutputted = prettyOutputs + "\n" + dataToBeOutputted;
-                  cout << dataToBeOutputted << endl;
                   
-                  ofstream outputFileStream(argv[2]);
-                  outputFileStream << dataToBeOutputted;
+                  string runConfig = promptForRunMode();
                   
-                  while (1) {
-                        unordered_map<string, bool> inputsNormalAndInverted;
-                        cout << "Enter boolean values for the following inputs:" << endl;
-                        cout << inputLabels.at(0);
-                        for (int i = 1; i < numInputs; i++) {
-                              cout << " " << inputLabels.at(i);
-                        }
-                        cout << endl;
-                        
-                        for (int i = 0; i < numInputs; i++) {
-                              string testForInputFiltering;
-                              cin >> testForInputFiltering;
-                              
-                              while (testForInputFiltering.compare("1") && testForInputFiltering.compare("0")) {
-                                    
-                                    transform(testForInputFiltering.begin(), testForInputFiltering.end(), testForInputFiltering.begin(), ::tolower);
-                                    
-                                    if (!testForInputFiltering.compare(":q")) {
-                                          return 0;
-                                    }
-                                    
-                                    cout << "Please try again..." << endl;
-                                    cin >> testForInputFiltering;
-                              }
-                              
-                              if (!testForInputFiltering.compare("1")) {
-                                    inputsNormalAndInverted[inputLabels.at(i)] = 1;
-                                    inputsNormalAndInverted[inputLabels.at(i) + "'"] = 0;
-                              }
-                              else if (!testForInputFiltering.compare("0")) {
-                                    inputsNormalAndInverted[inputLabels.at(i)] = 0;
-                                    inputsNormalAndInverted[inputLabels.at(i) + "'"] = 1;
-                              }
-                        }
-                        
-                        vector<bool> productsPostSim;
-                        for (int i = 0; i < numProducts; i++) {
-                              productsPostSim.push_back(1);
-                              for (int j = 0; j < products[i].size(); j++) {
-                                    if(!inputsNormalAndInverted[products[i][j]]){
-                                          productsPostSim.at(i) = 0;
-                                          break;
-                                    }
-                              }
-                        }
-                        
-                        unordered_map<string, bool> outputsPostSim;
-                        for (int i = 0; i < numOutputs; i++) {
-                              outputsPostSim[outputLabels[i][0]] = 0;
-                              for (int j = 1; j < outputLabels[i].size(); j++) {
-                                    if(productsPostSim.at(stoi(outputLabels[i][j]) - 1)){
-                                          outputsPostSim[outputLabels[i][0]] = 1;
-                                          break;
-                                    }
-                              }
-                              cout  << outputLabels[i][0] << ": "
-                              << outputsPostSim[outputLabels[i][0]] << endl;
-                        }
-                        
+                  if (!runConfig.compare("0") || !runConfig.compare("2")) {
+                        string inputFile = argv[1];
+                        string outputFile = inputFile.substr(0,inputFile.rfind(".txt")) + "_simplified.txt";
+                        ofstream outputFileStream(outputFile);
+                        outputFileStream << prettyOutputs << "\n" << dataToBeOutputted;
+                        outputFileStream.close();
                   }
+                  
+                  if (!runConfig.compare("1") || !runConfig.compare("2")) {
+                        return simulateCombinationalLogic(numInputs, numOutputs, numProducts, inputLabels, products, outputLabels);
+                  }
+                  
                   return 0;
             }
       }
       
       return 1;
+}
+
+string promptForRunMode() {
+      cout << "Get ready to simplify your Espresso output!\n\n";
+      
+      cout << "0) Generate a simplified Espresso output file" << endl;
+      cout << "1) Simulate combinational logic" << endl;
+      cout << "2) Perform functions 0 and 1" << endl;
+      cout << "q) Quit\n\n";
+      
+      cout << "Which function would you like to perform (0,1,2,q)? ";
+      string runConfig;
+      cin >> runConfig;
+      transform(runConfig.begin(), runConfig.end(), runConfig.begin(), ::tolower);
+      
+      return runConfig;
+}
+
+int simulateCombinationalLogic(int numInputs, int numOutputs, int numProducts, vector<string> inputLabels, vector<vector<string>> products, vector<vector<string>> outputLabels) {
+      while (1) {
+            unordered_map<string, bool> inputsNormalAndInverted;
+            cout << "\nEnter boolean values for the following inputs (enter q to quit):" << endl;
+            
+            cout << inputLabels.at(0);
+            for (int i = 1; i < numInputs; i++) {
+                  cout << " " << inputLabels.at(i);
+            }
+            cout << endl;
+            
+            for (int i = 0; i < numInputs; i++) {
+                  string testForInputFiltering;
+                  cin >> testForInputFiltering;
+                  
+                  while (testForInputFiltering.compare("1") && testForInputFiltering.compare("0")) {
+                        transform(testForInputFiltering.begin(), testForInputFiltering.end(), testForInputFiltering.begin(), ::tolower);
+                        
+                        if (!testForInputFiltering.compare("q")) {
+                              return 0;
+                        }
+                        
+                        cout << "Please try again..." << endl;
+                        cin >> testForInputFiltering;
+                  }
+                  
+                  // set boolean values for each input (both standard and inverted)
+                  if (!testForInputFiltering.compare("1")) {
+                        inputsNormalAndInverted[inputLabels.at(i)] = 1;
+                        inputsNormalAndInverted[inputLabels.at(i) + "'"] = 0;
+                  }
+                  else if (!testForInputFiltering.compare("0")) {
+                        inputsNormalAndInverted[inputLabels.at(i)] = 0;
+                        inputsNormalAndInverted[inputLabels.at(i) + "'"] = 1;
+                  }
+            }
+            
+            vector<bool> productsPostSim;
+            for (int i = 0; i < numProducts; i++) {
+                  productsPostSim.push_back(1);
+                  for (int j = 0; j < products[i].size(); j++) {
+                        if(!inputsNormalAndInverted[products[i][j]]){
+                              productsPostSim.at(i) = 0;
+                              break;
+                        }
+                  }
+            }
+            
+            unordered_map<string, bool> outputsPostSim;
+            for (int i = 0; i < numOutputs; i++) {
+                  outputsPostSim[outputLabels[i][0]] = 0;
+                  for (int j = 1; j < outputLabels[i].size(); j++) {
+                        if (productsPostSim.at(stoi(outputLabels[i][j]) - 1)) {
+                              outputsPostSim[outputLabels[i][0]] = 1;
+                              break;
+                        }
+                  }
+                  cout  << outputLabels[i][0] << ": "
+                  << outputsPostSim[outputLabels[i][0]] << endl;
+            }
+      }
 }
